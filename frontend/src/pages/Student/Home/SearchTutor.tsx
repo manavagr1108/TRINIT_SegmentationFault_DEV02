70f09a35ed9b537ea0714871415be5016275ac54
manavagr1108 <manavagr1108@gmail.com>
2024-03-09 12:14:51 +0530

    Fix: set state values for languages and priceRange;

--- frontend/src/pages/Student/Home/SearchTutor.tsx
+++ frontend/src/pages/Student/Home/SearchTutor.tsx
@@ -10,13 +10,18 @@ import {
   MultiSelect,
   RangeSlider,
   Select,
+  SimpleGrid,
+  Stack,
   Text,
   useCombobox,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import React, { useState } from "react";
 import SearchButton from "../../../components/SearchButton/SearchButton";
-import { languages as LANGUAGES } from "../../../utils/helpers";
+import {
+  languages as LANGUAGES,
+  showNotification,
+} from "../../../utils/helpers";
 import { searchTutor } from "../../../utils/apiCalls";
 
 function SearchTutor() {
@@ -25,46 +30,55 @@ function SearchTutor() {
   const [experience, setExperience] = useState<number>(0);
   const [priceRange, setPriceRange] = useState<[number, number]>([100, 5000]);
   const handleSearch = async () => {
+    if (languages.length === 0) {
+      showNotification("Error", "select atleast one language", "error");
+      return;
+    }
+    console.log(languages, experience, priceRange[0], priceRange[1]);
     const response = await searchTutor({
       languages,
       experience,
       lowerPrice: priceRange[0],
       upperPrice: priceRange[1],
     });
-    if(response.status === 200){
+    if (response.status === 200) {
       console.log(response);
     }
   };
 
   return (
     <Flex direction="column" className="w-fit">
-      <MultiSelect
-        label="Languages"
-        withAsterisk
-        placeholder="Select known languages"
-        data={LANGUAGES}
-        maw={400}
-      />
-      <Select
-        label="Experience"
-        withAsterisk
-        placeholder="required experience"
-        data={expArray}
-        value={expArray[experience]}
-        onChange={(v) =>
-          v === null ? setExperience(0) : setExperience(expArray.indexOf(v))
-        }
-        maw={100}
-      />
-      <Text mt="md">Set Min Max Prices: </Text>
-      <RangeSlider
-        defaultValue={[100, 5000]}
-        minRange={100}
-        min={100}
-        max={10000}
-        step={50}
-      />
-      <Button onClick={handleSearch}>Search Tutor</Button>
+      <Stack className="border-2 border-blue-400 rounded-lg p-6">
+        <MultiSelect
+          label="Languages"
+          withAsterisk
+          placeholder="Select known languages"
+          data={LANGUAGES}
+          onChange={(v) => setLanguages(v)}
+          maw={400}
+        />
+        <Select
+          label="Experience"
+          withAsterisk
+          placeholder="required experience"
+          data={expArray}
+          value={expArray[experience]}
+          onChange={(v) =>
+            v === null ? setExperience(0) : setExperience(expArray.indexOf(v))
+          }
+          maw={100}
+        />
+        <Text mt="md">Set Min Max Prices: </Text>
+        <RangeSlider
+          value={priceRange}
+          onChange={setPriceRange}
+          minRange={100}
+          min={100}
+          max={10000}
+          step={50}
+        />
+        <Button onClick={handleSearch}>Search Tutor</Button>
+      </Stack>
     </Flex>
   );
 }
